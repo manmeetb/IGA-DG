ee25e948344bf634f782bba5affbfb2570532825
manmeetb <manmeet.bhabra@mail.mcgill.ca>
2018-05-10 15:37:14 -0400

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # IGA-DG
 An Discontinuous Galerkin Flow Solver that incorporates IGA
+
+This flow solver has been coded in C++
---
+++ src/main.cpp
@@ -0,0 +1,16 @@
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    
+	string next_message = "This is the next message";
+
+	cout<<"Hello World"<<endl;
+    cout<<"Message : " << next_message<<endl;
+
+    return 0;
+}
+
